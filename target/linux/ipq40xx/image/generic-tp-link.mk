e10e7c4360447186c4f0e4346eacbf9bb5852a97
Bee Cadorna <r3usrlnd@gmail.com>
2025-08-07 21:31:35 -0400

    fix: remove v2 dts and replace with alt variant
    These are essentially the same hardware, so we don't need a separate DTS
    file for it.
    
    Signed-off-by: Bee Cadorna <r3usrlnd@gmail.com>

--- target/linux/ipq40xx/image/generic-tp-link.mk
+++ target/linux/ipq40xx/image/generic-tp-link.mk
@@ -13,23 +13,15 @@ define Device/tplink_deco-m5-v1
 	$(call Device/tplink-deco-m5-common)
 	DEVICE_MODEL := Deco-M5
 	DEVICE_VARIANT := v1
+	DEVICE_ALT0_VENDOR := TP-Link
+	DEVICE_ALT0_MODEL := Deco-M5
+	DEVICE_ALT0_VARIANT := v2
 	TPLINK_BOARD_ID := DECO-M5
 	SUPPORTED_DEVICES += tplink,deco-m5-v2
 	KERNEL_SIZE := 4096k
 	IMAGE_SIZE := 16640k
 endef
 TARGET_DEVICES += tplink_deco-m5-v1
-
-define Device/tplink_deco-m5-v2
-	$(call Device/tplink-deco-m5-common)
-	DEVICE_MODEL := Deco-M5
-	DEVICE_VARIANT := v2
-	TPLINK_BOARD_ID := DECO-M5
-	SUPPORTED_DEVICES += tplink,deco-m5-v1
-	KERNEL_SIZE := 4096k
-	IMAGE_SIZE := 16640k
-endef
-TARGET_DEVICES += tplink_deco-m5-v2
 
 define Device/tplink_deco-m5-v3
 	$(call Device/tplink-deco-m5-common)
